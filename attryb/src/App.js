0675ac2c7090f6de6e42af2b5c625c5c91a4b196
DhaanuI <dhaanu97@gmail.com>
2023-07-04 12:21:22 +0530

    created project and nav component partial

---
+++ attryb/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ attryb/src/components/sidebar.jsx
@@ -0,0 +1,33 @@
+const Sidebar = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+
+            <div className='subnav'>
+                <div className='content'>
+                    <div className='frame'>
+                        <h1>Settings</h1>
+                    </div>
+                    <div className='navigation'>
+                        <ul>
+                            <li>Personal Information</li>
+                            <li>Billing and Payment</li>
+                            <li>Domains</li>
+                            <li>User Management</li>
+                            <li>Customer Experience</li>
+                            <li>Page Structure</li>
+                            <li>Invite & Referrals</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
